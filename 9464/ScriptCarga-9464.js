897e1eb02938bf75cd2ae1b2891bb18c2ef0db2d
Erick <eopalomi@gmail.com>
2019-10-01 23:33:10 -0500

    commit Home

--- 9464/ScriptCarga-9464.js
+++ 9464/ScriptCarga-9464.js
@@ -15,15 +15,26 @@ var v_va_resqry  = DATA.SQL('wfacr', queryDatos, 1);
 
 /*LOGICA*/
 var valpagJson = new ValpagJson();
+MSG.PUSH_TO_USER(USUARI.co_usuari, MSG_TYPE_WARNING,'ALERTA',v_va_resqry.result, CO_CONTEN, true);
 
-for each (var rs in v_va_resqry.result){
+v_va_resqry.result.forEach(function(rs) {
     var rowx = new Row();
-    rowx.addReg(new Reg({co_pagreg:   10, va_pagreg: rs.no_banabo}));
-    rowx.addReg(new Reg({co_pagreg:   20, va_pagreg: rs.nu_cansol}));
-    rowx.addReg(new Reg({co_pagreg:   30, va_pagreg: rs.nu_candol}));
-    rowx.addReg(new Reg({co_pagreg:   40, va_pagreg: rs.im_totabo}));
+    rowx.addReg(new Reg({co_pagreg: 10, va_pagreg: rs.no_banabo}));
+    rowx.addReg(new Reg({co_pagreg: 20, va_pagreg: rs.nu_cansol}));
+    rowx.addReg(new Reg({co_pagreg: 30, va_pagreg: rs.nu_candol}));
+    rowx.addReg(new Reg({co_pagreg: 40, va_pagreg: rs.im_totabo}));
 	valpagJson.addRow(rowx);
-}
+});
+
+
+/* for (var rs in v_va_resqry.result) {
+    var rowx = new Row();
+    rowx.addReg(new Reg({co_pagreg:   10, va_pagreg: v_va_resqry.result[rs] }));
+    rowx.addReg(new Reg({co_pagreg:   20, va_pagreg: "rs.nu_cansol"}));
+    rowx.addReg(new Reg({co_pagreg:   30, va_pagreg: "rs.nu_candol"}));
+    rowx.addReg(new Reg({co_pagreg:   40, va_pagreg: "rs.im_totabo"}));
+	valpagJson.addRow(rowx);
+} */
 
 DO_POST_LOAD_DATA = function () {
     SHOWINFO(true);
